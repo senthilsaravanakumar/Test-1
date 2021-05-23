ee111709c77c25161c3cffb7335f08157547b4b0
senthilsaravanakumar <84587674+senthilsaravanakumar@users.noreply.github.com>
2021-05-23 19:31:25 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+function nextFunction()
+{
+    alert('The Next button will help navigating to the next page');
+}
+
+function prevFunction() {
+    alert('The Previous button will help navigating to the previous page');
+}
+
+function exitFunction() {
+    alert('EXIT THE COURSE!!!');
+}
+
